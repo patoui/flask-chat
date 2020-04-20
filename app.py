5fd889c6dc35f048b54ad93da25e0d01153ba6c5
Patrique Ouimet <patrique.ouimet@gmail.com>
2020-04-19 20:27:20 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, join_room, leave_room, send, emit
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+
+@socketio.on('json')
+def handle_json(json):
+    room = json['room']
+    send(json, json=True, room=room)
+
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', room=room)
+
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', room=room)
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, host='0.0.0.0')
